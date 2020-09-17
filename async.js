8f16a37145149e33f005bb52f656ebe17bf2ef35
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-09-17 14:15:07 -0600

    solution

--- async.js
+++ async.js
@@ -1,28 +1,50 @@
-  function showAvatar(name) {
+  async function showAvatar(name) {
   // read github user
-    let githubUser = axios.get(`https://api.github.com/users/${name}`);
-  
-    // show the avatar
-    document.getElementById('useravatar').src = githubUser.data.avatar_url;
-    document.getElementById('username').innerHTML = githubUser.data.name;
-  
+    try {
+      let githubUser = await axios.get(`https://api.github.com/users/${name}`);
+       console.log(githubUser)
+         // show the avatar
+      document.getElementById('useravatar').src = githubUser.data.avatar_url;
+      document.getElementById('username').innerHTML = githubUser.data.name;
+       
     return githubUser;
-
+       
+     } catch (error) {
+       console.log(`Error: ${error}`)
+     } finally {
+       console.log("This has been resolved")
+  }
 }
 
-showAvatar('subtlesnow');
 
- function showStargazers() {
-  // read github user
-  let githubUsers = axios.get(`
+
+// showAvatar('contheflan');
+
+async function showStargazers() {
+
+  try {
+      // read github user
+    let githubUsers = await axios.get(`
     https://api.github.com/repos/axios/axios/stargazers`);
-
+    // console.log(githubUsers.data)
+    githubUsers.data.forEach((user) => {
+      let img = document.createElement('img')
+      img.src = user.avatar_url
+      document.body.append(img)
+      let username = document.createElement('p')
+      username.textContent = user.login
+      document.body.append(username)
+    })
+    return githubUsers;
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
   // show the avatars
   // iterate over the response data
   // insert each avatar's image and username into the DOM.
   // choose whichever type of iterator for each git user..
 
-  return githubUsers;
+  
 }
 
-// showStargazers();
+showStargazers();
